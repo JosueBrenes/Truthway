98a9239e69008496827c148b054e1eba69da2cda
Josué Brenes <josuebrenes3005@gmail.com>
2023-04-09 20:06:56 -0600

    Truthway website

---
+++ Inicio/script.js
@@ -0,0 +1,11 @@
+/* Website icon */
+
+const favicon = document.getElementById("favicon");
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  favicon.setAttribute("href", "img/favicon-light.png");
+} else {
+  favicon.setAttribute("href", "img/favicon-dark.png");
+}
---
+++ Pagina principal/script.js
@@ -0,0 +1,37 @@
+$(function () {
+  $("a.page-scroll").bind("click", function (event) {
+    var $anchor = $(this);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr("href")).offset().top,
+        },
+        1500,
+        "easeInOutExpo"
+      );
+    event.preventDefault();
+  });
+});
+
+// Highlight the top nav as scrolling occurs
+$("body").scrollspy({
+  target: ".navbar-fixed-top",
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$(".navbar-collapse ul li a").click(function () {
+  $(".navbar-toggle:visible").click();
+});
+
+/* Website icon */
+
+const favicon = document.getElementById("favicon");
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  favicon.setAttribute("href", "img/favicon-light.png");
+} else {
+  favicon.setAttribute("href", "img/favicon-dark.png");
+}
