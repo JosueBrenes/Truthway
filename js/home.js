aebe207f58de26d1c0ecf017c562a4034d599282
Josué Brenes <josuebrenes3005@gmail.com>
2023-04-12 00:33:29 -0600

    Truthway website

--- js/home.js
+++ js/home.js
@@ -24,7 +24,7 @@ $(".navbar-collapse ul li a").click(function () {
   $(".navbar-toggle:visible").click();
 });
 
-/* Website icon */
+// Website icon //
 
 const favicon = document.getElementById("favicon");
 if (
@@ -35,3 +35,15 @@ if (
 } else {
   favicon.setAttribute("href", "./img/favicon-dark.png");
 }
+
+// Verse of the day //
+
+fetch("https://beta.ourmanna.com/api/v1/get/?format=json&order=random")
+  .then((response) => response.json())
+  .then((data) => {
+    const verseText = data.verse.details.text;
+    const verseReference = `${data.verse.details.reference} (RVR1960)`;
+    document.querySelector(".custom-text").textContent = verseText;
+    document.querySelector(".custom-reference").textContent = verseReference;
+  })
+  .catch((error) => console.error(error));
